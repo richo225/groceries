6afb5fe2343e916749ca245bc7086b687e4b65a6
Richard Bates <richo225@gmail.com>
2016-09-06 16:16:12 +0100

    Testing name change

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello Nativescript'></Label>"
+  template: "<Label text='hello Nativescrip'></Label>"
 })
 export class AppComponent {}
